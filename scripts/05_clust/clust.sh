0d46ebda60f1ef41d1efd5e3185a4eeb5c384207
Greenberg Michael <greenerkk@gmail.com>
2024-05-04 12:19:56 +0300

    Add analysis scripts

--- scripts/05_clust/clust.sh
+++ scripts/05_clust/clust.sh
@@ -3,11 +3,11 @@
 WORKING_DIR="/Volumes/oli/RESULTS/data/05_clust"
 cd ${WORKING_DIR}
 
-TPM_TABLE="/Volumes/oli/RESULTS/data/02_salmon/development_filtered_tpm_table.csv"
+TPM_TABLE="/Volumes/oli/RESULTS/data/02_salmon/filtered_tpm_table.csv"
 CLUST_REPLICATES="/Volumes/oli/RESULTS/scripts/05_clust/replicates_info.txt"
 CLUSTER_FORMATER="/Volumes/oli/RESULTS/scripts/05_clust/formate_clusters.py"
 
-CLUSTER_TIGHTNESS=10
+CLUSTER_TIGHTNESS=2
 
 source activate clust
 
